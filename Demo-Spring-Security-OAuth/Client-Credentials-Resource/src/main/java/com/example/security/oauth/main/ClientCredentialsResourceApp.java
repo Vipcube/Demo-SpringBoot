40603ef6def5b7fbd3fe628f97cdf8eea7b99698
Bread God <vipcube@gmail.com>
2019-10-28 13:59:40 +0800

    Fixed the rest controller not effect.
    Signed-off-by: Bread God <vipcube@gmail.com>

--- Demo-Spring-Security-OAuth/Client-Credentials-Resource/src/main/java/com/example/security/oauth/main/ClientCredentialsResourceApp.java
+++ Demo-Spring-Security-OAuth/Client-Credentials-Resource/src/main/java/com/example/security/oauth/main/ClientCredentialsResourceApp.java
@@ -3,11 +3,15 @@ package com.example.security.oauth.main;
 import com.example.security.oauth.config.AuthResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @Import( {
 		AuthResourceConfig.class
 } )
+@ComponentScan( basePackages = {
+		"com.example.security.oauth.controller"
+})
 @SpringBootApplication
 public class ClientCredentialsResourceApp {
 	public static void main( String[] args ) {
