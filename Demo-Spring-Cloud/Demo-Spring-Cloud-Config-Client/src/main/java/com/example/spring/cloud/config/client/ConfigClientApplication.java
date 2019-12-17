eaa3c250e2920b9bc4cc59491c991397c5f6b561
Bread God <vipcube@gmail.com>
2019-12-17 18:04:28 +0800

    First version of spring cloud config client.

---
+++ Demo-Spring-Cloud/Demo-Spring-Cloud-Config-Client/src/main/java/com/example/spring/cloud/config/client/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.example.spring.cloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main program.
+ *
+ * @author Brad Chen
+ *
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+	public static void main( String[] args ) {
+			SpringApplication.run( ConfigClientApplication.class, args );
+	}
+}
