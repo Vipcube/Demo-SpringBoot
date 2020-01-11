9b9e0edcbfee9b6a228e1a99e7a7d2d66a83032e
Bread God <vipcube@gmail.com>
2020-01-11 17:25:47 +0800

    Add the authorization code server demo.

---
+++ Demo-Spring-Security-OAuth/Authorization-Code-Server/src/main/java/com/example/security/oauth/config/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.example.security.oauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * The security config.
+ *
+ * @author Brad Chen
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure( HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				.authorizeRequests()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.formLogin();
+		// Use AuthorizationCode, need form login to protect the /oauth/authorize and /oauth/confirm_access
+	}
+
+	/**
+	 * The username and password for GrantTypes PasswordCredentials.
+	 *
+	 * @param builder
+	 * @throws Exception
+	 */
+	@Override
+	protected void configure( AuthenticationManagerBuilder builder) throws Exception {
+		builder.inMemoryAuthentication()
+				.passwordEncoder( passwordEncoder() )
+				.withUser("caterpillar")
+				.password( passwordEncoder().encode("12345678"))
+				.roles( "MEMBER" );
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+}
