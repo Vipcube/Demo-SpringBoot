ead1a6ee6345a94c05c26a5aac4b34f3f1c8bdd5
Bread God <vipcube@gmail.com>
2020-01-16 14:51:45 +0800

    Remove the unnecessary part.
    Signed-off-by: BradChen <vipcube@gmail.com>

--- Demo-Spring-Security-OAuth/Authorization-Code-Server/src/main/java/com/example/security/oauth/config/WebSecurityConfig.java
+++ Demo-Spring-Security-OAuth/Authorization-Code-Server/src/main/java/com/example/security/oauth/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure( HttpSecurity http) throws Exception {
 		http.csrf().disable()
 				.authorizeRequests()
-				.antMatchers("/oauth/**", "/login/**", "/logout/**")
+				.antMatchers("/login/**", "/logout/**")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
