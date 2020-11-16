7668b6b8d26d7fafbb1a142e7975f435f3f6a3cb
Bread God <Vipcube@users.noreply.github.com>
2020-11-16 23:36:09 +0800

    Finished with spring boot web demo project.

--- Demo-Spring-DifferentWithBoot/SpringBoot/src/main/java/org/example/spring/boot/AppBootInitializer.java
+++ Demo-Spring-DifferentWithBoot/SpringBoot/src/main/java/org/example/spring/boot/AppBootInitializer.java
@@ -3,6 +3,12 @@ package org.example.spring.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main program.
+ *
+ * @author Brad Chen
+ *
+ */
 @SpringBootApplication
 public class AppBootInitializer {
 	public static void main( String[] args ){
