b3ef14aad3345447c9ad97466ba9533729310c90
Bread God <Vipcube@users.noreply.github.com>
2021-09-27 16:22:52 +0800

    Add the spring security5 oauth sso login demo.

---
+++ Demo-Spring-Security5-OAuth/SSOLogin/src/main/java/org/example/spring/security/oauth/sso/SSOLoginApplication.java
@@ -0,0 +1,11 @@
+package org.example.spring.security.oauth.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSOLoginApplication {
+	public static void main( String[] args ){
+		SpringApplication.run( SSOLoginApplication.class, args );
+	}
+}
