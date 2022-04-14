6dded150e53adabe321fd0c81bdf5417efa7fe67
Bread God <Vipcube@users.noreply.github.com>
2022-04-14 18:40:55 +0800

    Update for clean repository layer without mapper annotation.

--- Demo-Spring-MyBatis/src/main/java/com/example/spring/boot/mybatis/MainApplication.java
+++ Demo-Spring-MyBatis/src/main/java/com/example/spring/boot/mybatis/MainApplication.java
@@ -1,8 +1,12 @@
 package com.example.spring.boot.mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan( {
+		"com.example.spring.boot.mybatis.mapper"
+} )
 @SpringBootApplication
 public class MainApplication {
 	public static void main( String[] args ){
