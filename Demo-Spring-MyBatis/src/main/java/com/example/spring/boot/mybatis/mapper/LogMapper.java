dcf240c1ed0e7877f27ddf9a50cdcc94069b7ac0
Bread God <Vipcube@users.noreply.github.com>
2022-04-14 17:19:19 +0800

    Update mapper with mapper annotation.

--- Demo-Spring-MyBatis/src/main/java/com/example/spring/boot/mybatis/mapper/LogMapper.java
+++ Demo-Spring-MyBatis/src/main/java/com/example/spring/boot/mybatis/mapper/LogMapper.java
@@ -1,8 +1,10 @@
 package com.example.spring.boot.mybatis.mapper;
 
 import com.example.spring.boot.mybatis.entity.Log;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+@Mapper
 @Repository
 public interface LogMapper {
 	Log selectById( long id );
--- Demo-Spring-MyBatis/src/main/java/com/example/spring/boot/mybatis/mapper/UserMapper.java
+++ Demo-Spring-MyBatis/src/main/java/com/example/spring/boot/mybatis/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.example.spring.boot.mybatis.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+@Mapper
 @Repository
 public interface UserMapper {
 	User selectById( long id );
