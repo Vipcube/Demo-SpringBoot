84f3aac274e2f032c072614f1be9796c64a88e4d
Bread God <Vipcube@users.noreply.github.com>
2022-04-24 15:27:54 +0800

    Improve code.

--- Demo-Spring-Docker/src/main/java/org/vipcube/spring/hello/MainApplication.java
+++ Demo-Spring-Docker/src/main/java/org/vipcube/spring/hello/MainApplication.java
@@ -2,11 +2,7 @@ package org.vipcube.spring.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan( basePackages = {
-	"com.example.demo.spring.docker"
-})
 @SpringBootApplication
 public class MainApplication {
 
--- Demo-Spring-Docker/src/main/java/org/vipcube/spring/hello/controller/HelloController.java
+++ Demo-Spring-Docker/src/main/java/org/vipcube/spring/hello/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.vipcube.spring.hello.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.vipcube.spring.hello.service.IHelloService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.constraints.NotBlank;
 
 @RestController
+@RequestMapping( "/api/hellos" )
 public class HelloController {
 	private final IHelloService service;
 
@@ -17,7 +19,7 @@ public class HelloController {
 		this.service = service;
 	}
 
-	@GetMapping( value = "/api/hellos/{name}" )
+	@GetMapping( value = "/{name}" )
 	public ResponseEntity<?> hello( @Validated @NotBlank @PathVariable("name") String name ) {
 		return ResponseEntity.ok( this.service.hello( name ) );
     }
