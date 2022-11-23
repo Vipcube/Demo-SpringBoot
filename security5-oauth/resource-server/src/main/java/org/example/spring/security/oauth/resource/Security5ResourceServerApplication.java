1af52d0263321d6b2875ee295de83370781cfde0
BradChen <vipcube@gmail.com>
2022-11-23 23:27:10 +0800

    refactor security demo

---
+++ security5-oauth/resource-server/src/main/java/org/example/spring/security/oauth/resource/Security5ResourceServerApplication.java
@@ -0,0 +1,11 @@
+package org.example.spring.security.oauth.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security5ResourceServerApplication {
+	public static void main( String[] args ){
+		SpringApplication.run( Security5ResourceServerApplication.class, args );
+	}
+}
---
+++ security5-oauth/sso-login/src/main/java/org/example/spring/security/oauth/sso/Security5SSOLoginApplication.java
@@ -0,0 +1,11 @@
+package org.example.spring.security.oauth.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security5SSOLoginApplication {
+	public static void main( String[] args ){
+		SpringApplication.run( Security5SSOLoginApplication.class, args );
+	}
+}
