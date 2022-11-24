a104a9a6973f279dc175c870d7de11d3370d42f2
Brad Chen <brad.chen@rybit.io>
2022-11-24 10:27:49 +0800

    Remove old eureka

---
+++ common-usage/eureka/src/main/java/org/vipcube/spring/common/eureka/config/WebSecurityConfiguration.java
@@ -0,0 +1,17 @@
+package org.vipcube.spring.common.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * If you want to allow endpoints, use this class. otherwise, auto-config is enough.
+ */
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
